6c65bfa3657aba86a5d26919c7e50865df0314f6
Ben Avison <code@piccolosystems.com>
2022-06-15 19:24:54 +0100

    Support generation of IAR project files using new CMake
    Note tinyusb_host_cdc_msc_hid example project requires file descriptors.
    This is the justification for using the full IAR C library by default.

--- cmake/preload/platforms/pico/pico.cmake
+++ cmake/preload/platforms/pico/pico.cmake
@@ -1,6 +1,11 @@
 if (NOT (DEFINED PICO_COMPILER OR DEFINED CMAKE_TOOLCHAIN_FILE))
-    pico_message("Defaulting PICO platform compiler to pico_arm_gcc since not specified.")
-    set(PICO_COMPILER "pico_arm_gcc")
+    if ("${CMAKE_GENERATOR}" STREQUAL "IAR Embedded Workbench for Arm")
+        pico_message("Defaulting PICO platform compiler to pico_arm_iar since not specified.")
+        set(PICO_COMPILER "pico_arm_iar")
+    else ()
+        pico_message("Defaulting PICO platform compiler to pico_arm_gcc since not specified.")
+        set(PICO_COMPILER "pico_arm_gcc")
+    endif ()
 endif ()
 
 
