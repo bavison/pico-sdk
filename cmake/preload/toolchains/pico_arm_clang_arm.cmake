c7015adad1a335ccadfdb223e09d266eacb6fcf0
Ben Avison <code@piccolosystems.com>
2022-10-24 13:50:02 +0100

    Add Arm DS build scripts
    Define appropriate build tool names and command-line switches for the Arm
    DS tools throughout CMake scripts across the tree.
    
    Use the pico_arm_clang_arm toolchain for this, on the assumption that was
    its intended use (as distinct from mainline Clang compilers, which I assume
    would use pico_arm_clang instead).
    
    Add link scripts ("scatter files") suitable for use with the armlink linker.

--- cmake/preload/toolchains/pico_arm_clang_arm.cmake
+++ cmake/preload/toolchains/pico_arm_clang_arm.cmake
@@ -10,10 +10,8 @@ set(CMAKE_SYSTEM_PROCESSOR cortex-m0plus)
 # Find ARMClang.
 pico_find_compiler(PICO_COMPILER_CC armclang)
 pico_find_compiler(PICO_COMPILER_CXX armclang)
-pico_find_compiler(PICO_COMPILER_ASM armasm)
-set(PICO_COMPILER_ASM "${PICO_COMPILER_ASM}" CACHE INTERNAL "")
-pico_find_compiler(PICO_OBJCOPY llvm-objcopy)
-pico_find_compiler(PICO_OBJDUMP llvm-objdump)
+set(PICO_COMPILER_ASM "${PICO_COMPILER_CC}" CACHE INTERNAL "")
+pico_find_compiler(PICO_OBJCOPY fromelf)
 
 # Specify the cross compiler.
 set(CMAKE_C_COMPILER ${PICO_COMPILER_CC} CACHE FILEPATH "C compiler")
@@ -25,7 +23,6 @@ set(CMAKE_ASM_COMPILER ${PICO_COMPILER_ASM} CACHE FILEPATH "ASM compiler")
 set(CMAKE_ASM_COMPILE_OBJECT "<CMAKE_ASM_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -o <OBJECT>   -c <SOURCE>")
 set(CMAKE_INCLUDE_FLAG_ASM "-I")
 set(CMAKE_OBJCOPY ${PICO_OBJCOPY} CACHE FILEPATH "")
-set(CMAKE_OBJDUMP ${PICO_OBJDUMP} CACHE FILEPATH "")
 
 # Disable compiler checks.
 set(CMAKE_C_COMPILER_FORCED TRUE)
@@ -42,7 +39,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 # todo move to platform/Generix-xxx
-set(ARM_CLANG_COMMON_FLAGS " --cpu=Cortex-M0plus")
+set(ARM_CLANG_COMMON_FLAGS " -mcpu=Cortex-M0plus --target=arm-arm-none-eabi")
 string(APPEND CMAKE_C_FLAGS_INIT "${ARM_CLANG_COMMON_FLAGS}")
 string(APPEND CMAKE_CXX_FLAGS_INIT "${ARM_CLANG_COMMON_FLAGS}")
 string(APPEND CMAKE_ASM_FLAGS_INIT "${ARM_CLANG_COMMON_FLAGS}")
