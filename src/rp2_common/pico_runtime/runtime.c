546c4c10c8b0bc7bd839374384b83dc3ac297de0
Ben Avison <code@piccolosystems.com>
2023-03-10 17:20:36 +0000

    Resolve __builtin_memcpy for non-GCC compilers
    IAR generates warning Pe223 'function "__builtin_memcpy" declared implicitly'
    and then error Li005 'no definition for "__builtin_memcpy"' at link time.
    
    It appears from a disassembly of the binary built by gcc 9.2.1 that this
    only resolving to a call to memcpy anyway, so it's not clear what the
    benefit in using __builtin_memcpy was - but I've left GCC calling it to be
    on the safe side.

--- src/rp2_common/pico_runtime/runtime.c
+++ src/rp2_common/pico_runtime/runtime.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 #include <sys/time.h>
 #include <sys/times.h>
 #include <unistd.h>
@@ -144,6 +145,9 @@ void runtime_init(void) {
     }
 
 #if !(PICO_NO_RAM_VECTOR_TABLE || PICO_NO_FLASH)
+#ifndef __GNUC__
+#define __builtin_memcpy memcpy
+#endif
     __builtin_memcpy(ram_vector_table, (uint32_t *) scb_hw->vtor, sizeof(ram_vector_table));
     scb_hw->vtor = (uintptr_t) ram_vector_table;
 #endif
