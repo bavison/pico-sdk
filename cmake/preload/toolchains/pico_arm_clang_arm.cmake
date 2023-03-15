a966a9873006156537420c4d9eabd7f0bdf8ad7c
Ben Avison <code@piccolosystems.com>
2023-03-15 16:50:15 +0000

    Add Arm DS build scripts
    Define appropriate build tool names and command-line switches for the Arm
    DS tools throughout CMake scripts across the tree.
    
    Use the pico_arm_clang_arm toolchain for this, on the assumption that was
    its intended use (as distinct from mainline Clang compilers, which I assume
    would use pico_arm_clang instead).
    
    Add link scripts ("scatter files") suitable for use with the armlink linker.

--- cmake/preload/toolchains/pico_arm_clang_arm.cmake
+++ cmake/preload/toolchains/pico_arm_clang_arm.cmake
@@ -10,10 +10,8 @@ set(CMAKE_SYSTEM_PROCESSOR cortex-m0plus)
 # Find ARMClang.
 pico_find_compiler(PICO_COMPILER_CC armclang)
 pico_find_compiler(PICO_COMPILER_CXX armclang)
-pico_find_compiler(PICO_COMPILER_ASM armasm)
-set(PICO_COMPILER_ASM "${PICO_COMPILER_ASM}" CACHE INTERNAL "")
-pico_find_compiler(PICO_OBJCOPY llvm-objcopy)
-pico_find_compiler(PICO_OBJDUMP llvm-objdump)
+set(PICO_COMPILER_ASM "${PICO_COMPILER_CC}" CACHE INTERNAL "")
+pico_find_compiler(PICO_OBJCOPY fromelf)
 
 # Specify the cross compiler.
 set(CMAKE_C_COMPILER ${PICO_COMPILER_CC} CACHE FILEPATH "C compiler")
@@ -25,7 +23,6 @@ set(CMAKE_ASM_COMPILER ${PICO_COMPILER_ASM} CACHE FILEPATH "ASM compiler")
 set(CMAKE_ASM_COMPILE_OBJECT "<CMAKE_ASM_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -o <OBJECT>   -c <SOURCE>")
 set(CMAKE_INCLUDE_FLAG_ASM "-I")
 set(CMAKE_OBJCOPY ${PICO_OBJCOPY} CACHE FILEPATH "")
-set(CMAKE_OBJDUMP ${PICO_OBJDUMP} CACHE FILEPATH "")
 
 # Add target system root to cmake find path.
 get_filename_component(PICO_COMPILER_DIR "${PICO_COMPILER_CC}" DIRECTORY)
@@ -39,5 +36,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 option(PICO_DEOPTIMIZED_DEBUG "Build debug builds with -O0" 0)
 
-set(ARM_TOOLCHAIN_COMMON_FLAGS " --cpu=Cortex-M0plus")
+set(ARM_TOOLCHAIN_COMMON_FLAGS " -mcpu=Cortex-M0plus --target=arm-arm-none-eabi")
 include(${CMAKE_CURRENT_LIST_DIR}/set_flags.cmake)
