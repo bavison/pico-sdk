1cce9310b0dd27780b85f21240a863ba5352a7ed
Ben Avison <code@piccolosystems.com>
2026-02-02 18:33:30 +0000

    Revert "Allow new_delete.cpp to build with non-GCC toolchains"
    This reverts commit 7106254ed25a320478c2922d9b68c10de8616e29.

--- src/rp2_common/pico_standard_link/new_delete.cpp
+++ src/rp2_common/pico_standard_link/new_delete.cpp
@@ -11,10 +11,6 @@
 #include <cstdlib>
 #include "pico.h"
 
-#ifndef __unused
-#define __unused __attribute__((__unused__))
-#endif
-
 void *operator new(std::size_t n) {
     return std::malloc(n);
 }
